ccacc4d7efcdbe021bd3bc9ca30e251f77c693c2
elyes <elyeskacemeducation@gmail.com>
2023-04-26 22:27:44 +0100

    finishing animation of translation

--- app/src/main/java/com/example/androidproject/DashboardActivity.java
+++ app/src/main/java/com/example/androidproject/DashboardActivity.java
@@ -1,26 +1,36 @@
 package com.example.androidproject;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class DashboardActivity extends AppCompatActivity {
-//    MaterialToolbar toolbar = findViewById(R.id.topAppBar);
-//    toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-//        @Override
-//        public void onClick(View v) {
-//            DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
-//            drawerLayout.openDrawer(GravityCompat.START);
-//        }
-//    });
+DrawerLayout drawerLayout;
+NavigationView navigationView;
+Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        drawerLayout=findViewById(R.id.drawerLayout);
+        navigationView=findViewById(R.id.naview);
+        toolbar=findViewById(R.id.topAppBar);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
     }
+
+
 }
