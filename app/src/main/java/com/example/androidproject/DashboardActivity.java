4c4c05b5e82750719711f942ff55f0e9f9bca0c2
elyes <elyeskacemeducation@gmail.com>
2023-04-26 11:18:45 +0100

    working on navbar

--- app/src/main/java/com/example/androidproject/DashboardActivity.java
+++ app/src/main/java/com/example/androidproject/DashboardActivity.java
@@ -3,9 +3,21 @@ package com.example.androidproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class DashboardActivity extends AppCompatActivity {
-
+    MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+//    toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+//            drawerLayout.openDrawer(GravityCompat.START);
+//        }
+//    });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
