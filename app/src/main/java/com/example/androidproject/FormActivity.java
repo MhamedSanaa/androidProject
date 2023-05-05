38aaa565ff6836570701055843e0157fdd523767
MhamedSanaa <mhamed.sanaa6@gmail.com>
2023-05-05 21:17:20 +0100

    fixing

--- app/src/main/java/com/example/androidproject/FormActivity.java
+++ app/src/main/java/com/example/androidproject/FormActivity.java
@@ -63,7 +63,6 @@ public class FormActivity extends Fragment implements OnMapReadyCallback {
 
     private FirebaseAuth mAuth;
 
-
     private GoogleMap mMap;
     private Marker mMarker;
     LatLng mLatLng = new LatLng(0, 0);
@@ -166,8 +165,6 @@ public class FormActivity extends Fragment implements OnMapReadyCallback {
                                             @Override
                                             public void onSuccess(DocumentReference documentReference) {
                                                 Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                                                sendNotification("New Posst","document"+documentReference.getId());
-
                                                 //Navigate to home page
 
 
@@ -253,31 +250,5 @@ public class FormActivity extends Fragment implements OnMapReadyCallback {
 
             }
         });
-    }
-
-    private void sendNotification(String title, String message) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(getContext(), "channel_id")
-                .setSmallIcon(R.drawable.baseline_notifications_none_24)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setAutoCancel(true);
-
-        Intent intent = new Intent(getContext(), MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(getContext(), 0, intent, PendingIntent.FLAG_IMMUTABLE);
-        builder.setContentIntent(pendingIntent);
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getContext());
-        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        notificationManager.notify(1, builder.build());
     }
 }
