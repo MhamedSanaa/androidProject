9914ae6282179609cae1c4e00bd8199e6d03308f
MhamedSanaa <mhamed.sanaa6@gmail.com>
2023-05-05 23:23:27 +0100

    joining functionality

--- app/src/main/java/adapter/PostAdapter.java
+++ app/src/main/java/adapter/PostAdapter.java
@@ -2,56 +2,51 @@ package adapter;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.androidproject.ModifyFragment;
 import com.example.androidproject.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import models.Post;
 
-public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder>{
-
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder> {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth;
     private List<QueryDocumentSnapshot> posts;
     private Context context;
-    Button modifyButton,joinButton,deleteButton;
 
     public PostAdapter(Context context, List<QueryDocumentSnapshot> posts) {
         this.context = context;
         this.posts = posts;
     }
+
     @NonNull
     @Override
     public PostAdapter.PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.post_card_view, parent, false);
-        modifyButton=view.findViewById(R.id.post_card_modify);
-        joinButton=view.findViewById(R.id.post_card_join);
-        modifyButton=view.findViewById(R.id.post_card_modify);
-
+        mAuth = FirebaseAuth.getInstance();
         return new PostViewHolder(view);
     }
 
@@ -59,54 +54,40 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
     public void onBindViewHolder(@NonNull PostAdapter.PostViewHolder holder, @SuppressLint("RecyclerView") int position) {
         QueryDocumentSnapshot queryDocumentSnapshot = posts.get(position);
 
-        String idOfPost=queryDocumentSnapshot.getString("UserId");
-//        Log.w("TAG+++++++++++++++++++++++++++++++++++++++++", idOfPost );
-//        Log.w("TAG-----------------------------------------",  FirebaseAuth.getInstance().getCurrentUser().getUid());
-        if(idOfPost.equals(FirebaseAuth.getInstance().getCurrentUser().getUid()))
-        {
+        String idOfPost = queryDocumentSnapshot.getString("UserId");
+        if (idOfPost.equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
             holder.joinButton.setVisibility(View.GONE);
-        }else{
+        } else {
             holder.modifyButton.setVisibility(View.GONE);
             holder.deleteButton.setVisibility(View.GONE);
         }
-
-
-        modifyButton.setOnClickListener(new View.OnClickListener() {
+        holder.joinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                QueryDocumentSnapshot queryDocumentSnapshot = posts.get(position);
-                String idOfPost = queryDocumentSnapshot.getId();
-                Post selectedPost=queryDocumentSnapshot.toObject(Post.class);
-
-
-                // Create a new instance of the ModifyFragment class
-                ModifyFragment modifyFragment = new ModifyFragment();
-
-                // Create a Bundle object to store the post ID
-                Bundle bundle = new Bundle();
-                bundle.putString("postId", idOfPost);
-                bundle.putString("subject", selectedPost.subject);
-                bundle.putString("date", selectedPost.date);
-                bundle.putString("time", selectedPost.time);
-                bundle.putString("address", selectedPost.address);
-                bundle.putString("latitude",Double.toString( selectedPost.latitude));
-                bundle.putString("longitude",Double.toString( selectedPost.longitude));
-
-                // Set the arguments of the ModifyFragment object to the Bundle object
-                modifyFragment.setArguments(bundle);
-
-                // Replace the current fragment with the ModifyFragment object
-                ((AppCompatActivity) context).getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_container, modifyFragment)
-                        .addToBackStack(null)
-                        .commit();
+                Log.w("aaaaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+                db.collection("posts")
+                        .document(queryDocumentSnapshot.getId())
+                        .get()
+                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                            @Override
+                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                List<String> joinersIds = (List<String>) documentSnapshot.get("joinersIds");
+                                if (joinersIds != null && joinersIds.contains(mAuth.getCurrentUser().getUid())) {
+                                    Log.w("join ereur", "user already a member");
+                                } else {
+                                    // Add the user to the member list and update the document
+                                    db.collection("posts")
+                                            .document(queryDocumentSnapshot.getId())
+                                            .update("joinersIds", FieldValue.arrayUnion(mAuth.getCurrentUser().getUid()));
+                                    Log.w("joining log", "user has joined");
+                                }
+                            }
+                        });
             }
         });
 
-
         Post post = queryDocumentSnapshot.toObject(Post.class);
-        Log.d( "onBindViewHolder: ---------------",post.UserId);
-//        holder.userIdTextView.setText(post.UserId);
+        Log.d("onBindViewHolder: ---------------", post.UserId);
         holder.date.setText(post.date);
         holder.userTextView.setText(post.user);
         holder.subjectTextView.setText(post.subject);
@@ -129,18 +110,15 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
     public int getItemCount() {
         return posts.size();
     }
+
     public static class PostViewHolder extends RecyclerView.ViewHolder {
-//        private TextView userIdTextView;
         private TextView date;
         private TextView userTextView;
         private TextView subjectTextView;
         private TextView time;
         private TextView numOfPart;
         private TextView post_card_location;
-
         private MapView mapView;
-
-
         private Button modifyButton;
         private Button joinButton;
         private Button deleteButton;
@@ -156,9 +134,9 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
             time = itemView.findViewById(R.id.post_card_time);
             numOfPart = itemView.findViewById(R.id.post_card_participation);
             post_card_location = itemView.findViewById(R.id.post_card_location);
-            modifyButton=itemView.findViewById(R.id.post_card_modify);
-            joinButton=itemView.findViewById(R.id.post_card_join);
-            deleteButton=itemView.findViewById(R.id.post_card_delete);
+            modifyButton = itemView.findViewById(R.id.post_card_modify);
+            joinButton = itemView.findViewById(R.id.post_card_join);
+            deleteButton = itemView.findViewById(R.id.post_card_delete);
             if (mapView != null) {
                 mapView.onCreate(null);
                 mapView.onResume();
